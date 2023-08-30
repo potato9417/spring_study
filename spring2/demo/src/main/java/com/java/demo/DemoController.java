b39e44fa5b255986f70afdb89d1cb35cf35cad0c
potato9417 <59465217+potato9417@users.noreply.github.com>
2023-08-30 15:26:55 +0900

    0830_전자정부프레임워크 기초

---
+++ spring2/demo/src/main/java/com/java/demo/DemoController.java
@@ -0,0 +1,28 @@
+package com.java.demo;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	
+	private static final Logger log = LoggerFactory.getLogger(DemoController.class); 
+	
+	@GetMapping("/demo.do")
+	public String demo(HttpServletRequest request, Model model) {
+		
+		log.info("--- demo.do ---");
+		
+		// 인자 => JSP(forward 방식)
+		request.setAttribute("arg1", 11);
+		request.setAttribute("arg2", 17);
+		
+		return "demo"; // demo.jsp
+		
+	}	
+}
---
+++ spring2/demo2/src/main/webapp/WEB-INF/views/demo.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+arg1 : ${arg1}<br>
+arg2 : ${arg2}<br>
+</body>
+</html>
